f189515c4f2785c20147ce6097916b2faf52605b
Lyosha0i <lyosha_0@mail.ru>
2021-12-24 17:28:36 +0400

    Added Task1.1.10.

---
+++ Task 1/Task1.1.10/DDArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum=0;
+            int[,] a = new int[n,n];
+            Random r = new Random();
+            for (int i = 0; i < a.GetLength(0); i++) for (int j = 0; j < a.GetLength(1); j++) a[i, j] = r.Next(10);
+            for (int i = 0; i < a.GetLength(0); i++) {
+                for (int j = 0; j < a.GetLength(1); j++) Console.Write("{0} ",a[i,j]);
+                Console.WriteLine();
+                }
+            for (int i = 0; i < a.GetLength(0); i++) for (int j = 0; j < a.GetLength(1); j++) if ((i + j) % 2 == 0) sum += a[i, j];
+                    Console.WriteLine(sum);
+        }
+    }
+}
