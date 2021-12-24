82214bbdafa9637d6b958acf083b0cf16191046e
Lyosha0i <lyosha_0@mail.ru>
2021-12-24 16:05:48 +0400

    Added Task1.1.3.

---
+++ Task 1/Task1.1.3/Another.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Triangle(int n)
+        {
+            for (int i = 1; i <= n; i++){
+                for (int j = n - i; j >= 1; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 1; j <=i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+                }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n=");
+            int n = int.Parse(Console.ReadLine());
+            Triangle(n);
+            Console.ReadKey(true);
+        }
+    }
+}
