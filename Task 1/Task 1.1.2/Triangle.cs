9fa788aca260d8e229d0e02e86d869361e815155
Lyosha0i <lyosha_0@mail.ru>
2021-12-24 16:04:59 +0400

    Added Task1.1.2

---
+++ Task 1/Task 1.1.2/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        public static void Triangle(int n)
+        {
+            for (int i = 1; i <= n; i++){
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n=");
+            int n = int.Parse(Console.ReadLine());
+            Triangle(n);
+        }
+    }
+}
