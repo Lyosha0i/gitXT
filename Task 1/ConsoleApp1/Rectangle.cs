ee285cd6071ee7f7c6d07f92421e44dfd1b0b773
Lyosha0i <lyosha_0@mail.ru>
2021-12-24 16:02:47 +0400

    Added Task 1.1.1

---
+++ Task 1/ConsoleApp1/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Square(int a,int b)
+        {
+            if (a > 0 && b > 0) Console.WriteLine(a*b);
+            else Console.WriteLine("Invalid");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b=");
+            int b = int.Parse(Console.ReadLine());
+            Square(a,b);
+        }
+    }
+}
