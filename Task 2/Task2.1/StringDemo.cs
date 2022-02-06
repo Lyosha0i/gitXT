b32122e629b7436c6bc4f0aec1359d9af2715b6f
Lyosha0i <lyosha_0@mail.ru>
2022-02-06 20:34:55 +0400

    Made the code more comfortable.

--- Task 2/Task2.1/StringDemo.cs
+++ Task 2/Task2.1/StringDemo.cs
@@ -6,32 +6,37 @@ namespace Task2._1
 {
     class StringDemo
     {
-        private char[] c;
-        public char[] C{
-            get { return c; }
+        private char[] charArray;
+        public char[] CharArray{
+            get { return charArray; }
 }
         public StringDemo(char[] c)
         {
-            this.c = c;
+            this.charArray = c;
         }
         public static bool Compare(StringDemo a, StringDemo b)
         {
-            if (a.c.Length != b.c.Length) return false;
-            for (int i = 0; i < a.c.Length; i++)if (a.c[i] != b.c[i]) return false;
+            if (a.charArray.Length != b.charArray.Length)
+                return false;
+            for (int i = 0; i < a.charArray.Length; i++)
+                if (a.charArray[i] != b.charArray[i])
+                    return false;
             return true;
         }
         public static StringDemo Concat(params StringDemo[] arr)
         {
             int n=0;
-            foreach (var i in arr)n+=i.c.Length;
+            foreach (var i in arr)
+                n+=i.charArray.Length;
             char[] c = new char[n];
             StringDemo a = new StringDemo(c);
             int j=0;
-            foreach (var i in arr) while (j < n)
+            foreach (var i in arr)
+                while (j < n)
                 {
-                    for (int k = 0; k < i.c.Length; k++)
+                    for (int k = 0; k < i.charArray.Length; k++)
                     {
-                        a.c[j] = i.c[k];
+                        a.charArray[j] = i.charArray[k];
                         j++;
                         //Console.WriteLine(i.c);
                     }
@@ -41,31 +46,37 @@ namespace Task2._1
         }
         public static int Search(StringDemo s, char cc)
         {
-            for (int i = 0; i < s.c.Length; i++)
+            for (int i = 0; i < s.charArray.Length; i++)
             {
-                if (s.c[i] == cc) return i + 1;
+                if (s.charArray[i] == cc)
+                    return i + 1;
             }
             return -1;
         }
         public static int Search(StringDemo s, char cc, int start=1)
         {
-            for (int i = start-1; i < s.c.Length; i++)
+            for (int i = start-1; i < s.charArray.Length; i++)
             {
-                if (s.c[i] == cc) return i + 1;
+                if (s.charArray[i] == cc)
+                    return i + 1;
             }
             return -1;
         }
         public static char[] StrDToCharArr(StringDemo s){
-            return s.c;
+            return s.charArray;
         }
         public StringDemo Replace(char c1,char c2) {
-            for (int i = 0; i < this.c.Length; i++)if(this.c[i]==c1)this.c[i]=c2;
+            for (int i = 0; i < this.charArray.Length; i++)
+                if(this.charArray[i]==c1)
+                    this.charArray[i]=c2;
             return this;
         }
         public static int Count(StringDemo s, char cc)
         {
             int a=0;
-            for (int i = 0; i < s.c.Length; i++) if (s.c[i] == cc) a++;
+            for (int i = 0; i < s.charArray.Length; i++)
+                if (s.charArray[i] == cc)
+                    a++;
             return a;
         }
     }
