f64c7d2d57d4628037aba587ee43a6868a498ff1
Lyosha0i <lyosha_0@mail.ru>
2022-05-29 11:58:49 +0400

    Added Task 8.2.

---
+++ Task 8/Task8.1/Task8.1.1.BLL/Models/AddModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для Class1
+/// </summary>
+public class AddModel
+{
+    public string user;
+    public static readonly string add = "The user is added";
+    public static readonly string date = "Incorrect date";
+    private static AddModel _instance;
+    public string Day;
+    public string Month;
+    public string Year;
+    public string Name;
+    public static AddModel Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new AddModel();// (0,null);
+            return _instance;
+        }
+        set
+        {
+            if (_instance != null)//&&_instance.counter == 0)
+                _instance = value;
+        }
+    }
+}
---
+++ Task 8/Task8.1/Task8.1.1.Entities/UserAward.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._1._1.Entities
+{
+    public class UserAward
+    {
+        public Guid ID { get; set; }
+        public User UserProp { get; set; }
+        public Award AwardProp { get; set; }
+
+        //public UserAward this[Guid index]
+        //{
+        //    get => personal[index];
+        //    set => personal[index] = value;
+        //}
+        public UserAward(Guid Id, User user, Award award)
+        {
+            ID = Id;
+            UserProp = user;
+            AwardProp = award;
+        }
+        public UserAward(User user, Award award)
+        {
+            ID = Guid.NewGuid();
+            UserProp = user;
+            AwardProp = award;
+        }
+        public UserAward()
+        {
+
+        }
+    }
+}
