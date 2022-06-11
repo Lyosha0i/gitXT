1eec57ad2036b21861ba471138db5d60b10c9f6e
Lyosha0i <lyosha_0@mail.ru>
2022-06-11 08:39:05 +0400

    Fixed PL in the Task 8.1.

--- Task 8/Task8.1/Task8.1.1.BLL/Models/DeleteModel.cs
+++ Task 8/Task8.1/Task8.1.1.BLL/Models/DeleteModel.cs
@@ -6,12 +6,14 @@ using System.Web;
 /// <summary>
 /// Сводное описание для DeleteModel
 /// </summary>
-public class DeleteModel
+public static class DeleteModel
 {
-    public DeleteModel()
-    {
-        //
-        // TODO: добавьте логику конструктора
-        //
-    }
+    public static Guid guid=new Guid();
+    public static string title;
+    //public DeleteModel()
+    //{
+    //    //
+    //    // TODO: добавьте логику конструктора
+    //    //
+    //}
 }
